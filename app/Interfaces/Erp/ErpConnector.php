08b8eb54c4b9df80b187e9901b10a1faf5e8daf9
Marco Casali <marco.casali13@studio.unibo.it>
2024-06-07 11:17:00 +0200

    created erp connector interface

--- app/Interfaces/Erp/ErpConnector.php
+++ app/Interfaces/Erp/ErpConnector.php
@@ -6,7 +6,7 @@ namespace App\Interfaces\Erp;
  * Connector to communicate with the
  * ERP through SOAP web services
  */
-interface ErpSoapConnector
+interface ErpConnector
 {
     /**
      * Creates a Prospect in the ERP
