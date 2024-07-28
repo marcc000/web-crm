919de649c623dfbfb7e7e3d7fcf70c62890e8513
Marco Casali <marco.casali13@studio.unibo.it>
2024-07-28 11:12:55 +0200

    feat: user seeding from json file

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = Storage::json("seeds/users.json",);
+
+        foreach($users as $user) {
+            User::create([
+                "name" => $user["name"],
+                "email" => $user["email"],
+                "password" => Hash::make($user["password"]),
+                "erp_id" => $user["erp_id"],
+            ])
+            ->assignRole($user["roles"]);
+        }
+    }
+}
