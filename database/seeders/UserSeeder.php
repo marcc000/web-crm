f66657b02928b9343f1704f3bc276bd924a6fa16
marcc000 <marcc000@users.noreply.github.com>
2024-07-29 18:36:01 +0000

    Fixes coding style

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,10 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -17,16 +17,16 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = Storage::json("seeds/users.json",);
+        $users = Storage::json('seeds/users.json');
 
-        foreach($users as $user) {
+        foreach ($users as $user) {
             User::create([
-                "name" => $user["name"],
-                "email" => $user["email"],
-                "password" => Hash::make($user["password"]),
-                "erp_id" => $user["erp_id"],
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password']),
+                'erp_id' => $user['erp_id'],
             ])
-            ->assignRole($user["roles"]);
+                ->assignRole($user['roles']);
         }
     }
 }
